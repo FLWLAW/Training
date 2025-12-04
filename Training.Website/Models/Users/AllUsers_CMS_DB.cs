27c3efb76d2917e006db4da2b843a8589db0bc51
Drosenblum53 <DRosenblum@flwlaw.com>
2025-12-04 16:09:10 -0500

    Adding Emailings to DB.

--- Training.Website/Models/Users/AllUsers_CMS_DB.cs
+++ Training.Website/Models/Users/AllUsers_CMS_DB.cs
@@ -7,6 +7,7 @@
         public int? TitleID { get; set; }
         public string? UserName { get; set; }
         public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public string? EmailAddress { get; set; }
         public string? LoginID { get; set; }
     }
