594cb836de84d27a74b29d98816ea7b90ee71f39
Drosenblum53 <DRosenblum@flwlaw.com>
2025-12-24 10:40:57 -0500

    Renamed "Stages" to "Worklist Groups."

---
+++ Training.Website/Models/WorklistGroupsAndReportsModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Training.Website.Models
+{
+    public class WorklistGroupsAndReportsModel
+    {
+        // FOR SP [usp_StageList_SAByStateListReportIDList]
+
+        public string? ReportName { get; set; }
+        public string? AssignedUserList { get; set; }
+        public string? TempAssignedUserList { get; set; }
+        public string? StageFullName { get; set; }
+    }
+}
--- Training.Website/Services/AudienceServiceMethods.cs
+++ Training.Website/Services/AudienceServiceMethods.cs
@@ -9,19 +9,15 @@ namespace Training.Website.Services
         public async Task<IEnumerable<IdValue<int>?>?> GetAllReports(IDatabase? database) =>
             await database!.QueryByStoredProcedureForDropDownControlAsync<int>("usp_Report_SA", "ReportID", "ReportDesc");
 
-        public async Task<IEnumerable<StagesReportsModel?>?> GetAllStages(IDatabase? database)
+        public async Task<IEnumerable<WorklistGroupsAndReportsModel?>?> GetAllWorklistGroupsWithReports(IDatabase? database)
         {
             StagesReportsModel_Parameters parameters = new();   // LEAVE PROPERTIES AS NULL
-
-            IEnumerable<StagesReportsModel?> results = await database!.QueryByStoredProcedureAsync<StagesReportsModel?, StagesReportsModel_Parameters?>("usp_StageList_SAByStateListReportIDList", parameters);
-            //await database!.QueryByStoredProcedureAsync<StageModel?>("usp_StageList_SA");
+            IEnumerable<WorklistGroupsAndReportsModel?> results =
+                await database!.QueryByStoredProcedureAsync<WorklistGroupsAndReportsModel?, StagesReportsModel_Parameters?>("usp_StageList_SAByStateListReportIDList", parameters);
 
             return results;
         }
-        /*
-        public async Task<IEnumerable<Reports_Username_ReportDesc_StageName_Model?>?> GetReportsUsersReportDescriptionsStageNames(IDatabase? database) =>
-            await database!.QueryByStoredProcedureAsync<Reports_Username_ReportDesc_StageName_Model?>("usp_ReportSelectionStage_SA");
-        */
+
         public void UpsertEMailingRecord(AllUsers_Assignment? recipient, int? session_ID, string? sendingUser, IDatabase? database)
         {
             UpsertEMailings_Parameters parameters = new()
