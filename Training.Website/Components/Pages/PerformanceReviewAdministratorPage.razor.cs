d19f9c915aa800f6c95531eb20542243358c58dd
Drosenblum53 <DRosenblum@flwlaw.com>
2026-02-02 17:22:32 -0500

    Adding code to Performance Review Administrator page.

--- Training.Website/Components/Pages/PerformanceReviewAdministratorPage.razor.cs
+++ Training.Website/Components/Pages/PerformanceReviewAdministratorPage.razor.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using SqlServerDatabaseAccessLibrary;
+using Telerik.Blazor.Components;
+using Training.Website.Models;
 using Training.Website.Models.Reviews;
 using Training.Website.Services;
 
@@ -24,8 +26,11 @@ namespace Training.Website.Components.Pages
 
         #region PRIVATE FIELDS
         private int? _selectedReviewYear = null;
+        private string? _selectedAnswerFormatDropDownValue = null;
         private string[]? _reviewYears = null;
-        private List<PerformanceReviewQuestionModel?>? _questions = null;
+        private Dictionary<int, string>? _answerFormats = null;
+        private List<PerformanceReviewQuestionModel?>? _activeQuestions = null;
+        private List<PerformanceReviewQuestionModel?>? _deletedQuestions = null;
         private PerformanceReviewServiceMethods _service = new();
         #endregion
 
@@ -33,17 +38,52 @@ namespace Training.Website.Components.Pages
         protected override async Task OnInitializedAsync()
         {
             _reviewYears = ReviewYears();
-            await base.OnInitializedAsync();        //TODO: DELETE THIS LINE ONCE THERE IS AWAITABLECODE IN IT.
+            //_answerFormats = (await _service.GetPerformanceReviewAnswerFormats_AdministratorPage(Database_OPS))?.ToArray();
+            _answerFormats = await _service.GetPerformanceReviewAnswerFormats(Database_OPS);
         }
 
         // =========================================================================================================================================================================================================================================================================================================
+
+        private void ActiveQuestionCreateHandler(GridCommandEventArgs args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void ActiveQuestionDeleteHandler(GridCommandEventArgs args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void ActiveQuestionUpdateHandler(GridCommandEventArgs args)
+        {
+            var updatedQuestion = (PerformanceReviewQuestionModel?)args.Item;
+
+            if (updatedQuestion != null && string.IsNullOrWhiteSpace(_selectedAnswerFormatDropDownValue) == false)
+            {
+                foreach(KeyValuePair<int, string> kvp in _answerFormats!)
+                {
+                    if (kvp.Value == _selectedAnswerFormatDropDownValue)
+                    {
+                        updatedQuestion.AnswerFormat = kvp.Key;
+                        break;
+                    }
+                }
+                _selectedAnswerFormatDropDownValue = null;
+            }
+        }
+
+        private void RestoreHandler(GridCommandEventArgs args)
+        {
+            //throw new NotImplementedException();
+        }
 
         private async Task ReviewYearChanged(string newValue)
         {
             if (int.TryParse(newValue, out int selectedReviewYear) == true)
             {
                 _selectedReviewYear = selectedReviewYear;
-                _questions = (await _service.GetPerformanceReviewQuestions(_selectedReviewYear.Value, false, Database_OPS))?.ToList();
+                _activeQuestions = (await _service.GetPerformanceReviewQuestions(_selectedReviewYear.Value, false, Database_OPS))?.ToList();
+                _deletedQuestions = (await _service.GetPerformanceReviewQuestions(_selectedReviewYear.Value, true, Database_OPS))?.ToList();
                 /*
                 _allRadioChoices = (await _service.GetAllRadioButtonChoicesByYear(_selectedReviewYear.Value, Database_OPS))?.ToArray();
                 _selectedUser = null;
